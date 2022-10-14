4800f33bc493ccf7391024b433b4f0cd3729a80c
Vishal <vishal.s.kumar99@gmail.com>
2022-10-15 00:30:02 +0530

    skills screen changes

--- src/screens/Skills.js
+++ src/screens/Skills.js
@@ -16,21 +16,23 @@ function Skills() {
             </Col>
             <Col sm>
                 <div className="animate__animated animate__bounceInDown">
-                    <h1 className="display-4">Technical<span role="img" aria-label="trophy">🏆</span></h1>
+                    <h1 className="display-4 color-cycle">Technical<span role="img" aria-label="trophy">🏆</span></h1>
                     <ul>
-                        <li>Backend: Express, Flask, Django</li>
-                        <li>Frontend: React, Vue</li>
+                        <li>REST APIs</li>
+                        <li>Databases: Mongodb, Couchbase, Oracle, Postgres</li>
+                        <li>Back-end: Express, Flask, Django, Spring MVC</li>
+                        <li>Front-end: React, Vue, Angular, ExtJS</li>
                         <li>Mobile dev: React Native, Android Studio</li>
                         <li>Electron</li>
-                        <li>REST APIs</li>
                     </ul>
                 </div>
                 <div className="animate__animated animate__bounceInUp">
-                    <h1 className="display-4">Non-technical<span role="img" aria-label="guitar">🎸</span></h1>
+                    <h1 className="display-4 color-cycle">Non-technical<span role="img" aria-label="guitar">🎸</span></h1>
                     <ul>
                         <li>Chess</li>
                         <li>Guitar</li>
                         <li>Music Composition</li>
+                        <li>Valorant (peak diamond 3)</li>
                     </ul>
                     <p className="display-6">Honestly, there's a lot more. This page wouldn't be enough.</p>
                 </div>
